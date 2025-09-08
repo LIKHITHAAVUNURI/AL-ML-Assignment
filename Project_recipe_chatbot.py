6e3b1434950b4323cab2b22696c8d25a2bd32e7e
LIKHITHAAVUNURI <114133448+LIKHITHAAVUNURI@users.noreply.github.com>
2025-09-08 11:03:17 +0530

    Add files via upload
    Project_recipe_chatbot.py

---
+++ Project_recipe_chatbot.py
@@ -0,0 +1,17 @@
+Task 2: Local LLM Integration & Chatbot.
+
+Project: recipe_chatbot
+
+recipe_chatbot/
+│
+├── app/
+│   ├── model/
+│   │   ├── model_loader.py
+│   │   └── recipe_data.json
+│   ├── api/
+│   │   └── main.py
+│   └── chatbot/
+│       └── cli_chat.py
+│
+├── requirements.txt
+└── README.md
